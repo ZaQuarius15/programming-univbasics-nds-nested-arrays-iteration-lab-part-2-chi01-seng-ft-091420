920ee14ae8072a39e798f67160e1e40906a3d386
Zack Carlson <jzcarlson014@gmail.com>
2020-08-25 02:37:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,8 @@ def find_min_in_nested_arrays(src)
   sanja = 0 
   while sanja < src.count do
     zack = 0
-    if src[sanja][zack] < 
+    while zack < src[sanja].count do
+      if src[sanja][zack] < 
       
     zack = zack + 1 
 end
