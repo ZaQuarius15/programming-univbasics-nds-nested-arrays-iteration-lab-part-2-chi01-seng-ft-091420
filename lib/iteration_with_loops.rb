b3d5ef4340e7ada839f85678abacd214639605fc
Zack Carlson <jzcarlson014@gmail.com>
2020-08-25 02:43:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,19 @@
 def find_min_in_nested_arrays(src)
+  new_array = []
   sanja = 0 
   while sanja < src.count do
     zack = 0
     largest = false
     while zack < src[sanja].count do
-      if src[sanja][zack] is false
-        largest =
-      
-    zack += 1 
+      if largest == false
+        largest = src[sanja][zack]
+      elsif largest < src[sanja][zack]
+        largest = src[sanja][zack]
+      new_array.append(largest)
+      zack += 1 
+    end
+    sanja += 1 
+  end
+  new_array
 end
 
