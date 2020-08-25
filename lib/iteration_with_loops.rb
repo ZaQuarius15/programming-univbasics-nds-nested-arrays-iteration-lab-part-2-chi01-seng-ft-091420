add3add5d4b230db01ee2f0d612e5e9ae1133351
Zack Carlson <jzcarlson014@gmail.com>
2020-08-25 02:31:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,9 @@
 def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  sanja = 0 
+  while sanja < src.count do
+    zack = 0
+    if src[sanja][zack] < 
+      
+    zack = zack + 1 
 end
+
